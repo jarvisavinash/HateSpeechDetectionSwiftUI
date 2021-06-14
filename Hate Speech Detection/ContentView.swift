f852d5231363cea6e1c996899caadbc6c4defc49
Avinash Patil <jarvis.avinash@gmail.com>
2021-06-14 11:50:34 +0530

    Initial Commit

---
+++ Hate Speech Detection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hate Speech Detection
+//
+//  Created by Avinash on 14/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
